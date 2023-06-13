291c0e2c25cba16b0ee226e2a545a7f4f845c9a5
Lucas Lee <luclee0808@gmail.com>
2023-06-13 00:07:38 -0700

    changed text for credits and instructions

--- src/scenes/Instructions.js
+++ src/scenes/Instructions.js
@@ -14,8 +14,12 @@ class Instructions extends Phaser.Scene {
         this.bg = this.add.image(gameWidth/2, gameHeight/2, 'bg').setOrigin(0)
         this.add.text(15, 20, 'First Scene - Use arrow keys to move.',subText)
         this.add.text(25, 40,'Escape the town before its too late', subText)
-        this.add.text(15, 70, 'Second and Third Scenes - Use arrow keys to move.', subText)
-        this.add.text(25, 90, 'Space to interact when possible', subText)
+        this.add.text(15, 70, 'Second Scene - Use arrow keys to move.', subText)
+        this.add.text(25, 90, 'Look for Setsuko for further instructions', subText)
+        this.add.text(15, 120, 'Third Scene - Use arrow keys to move', subText)
+        this.add.text(25, 140, 'Space bar to Interact when needed', subText)
+
+
 
         this.add.text(90, 200, 'Press B to go back to Main Menu', credText)
         
